3cfd62a745c9998230b243ffdd5afb6bada73a5e
PavelChernickov <chernickov.pasha@gmail.com>
2024-09-06 13:04:32 +0300

    2

--- client/src/redux/thunkActions.ts
+++ client/src/redux/thunkActions.ts
@@ -3,6 +3,7 @@ import axiosInstance, { setAccessToken } from "../../axiosInstance";
 import { AxiosResponse } from "axios";
 import { IEntrie, IUser} from './types/stateTypes';
 const { VITE_API }: ImportMeta['env'] = import.meta.env;
+import { initUserState } from "./initStates/initStates";
 
 export const getEntries = createAsyncThunk('entries/all', async (): Promise<IEntrie[]> => {
   const { data }: AxiosResponse<IEntrie[]> = await axiosInstance.get(`${VITE_API}/game/deck`)
