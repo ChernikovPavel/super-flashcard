6006460de7248d3ba442cb9f9cdac5dedf884fa4
PavelChernickov <chernickov.pasha@gmail.com>
2024-09-06 17:20:05 +0300

    1

--- server/db/seeders/20240905100711-User.js
+++ server/db/seeders/20240905100711-User.js
@@ -1,7 +1,5 @@
 "use strict";
 const bcrypt = require("bcrypt");
-"use strict";
-const bcrypt = require("bcrypt");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -12,13 +10,14 @@ module.exports = {
     const salt = Math.floor(Math.random() * 10)
     const max = salt + mockNames.length 
     for(let i = salt; i < max; i+=1) mixedMockNames.push(mockNames[i % mockNames.length])
+
     for (let i = 1; i <= 4; i += 1) {
       arr.push({
         name: mixedMockNames[i % 4],
         email: `${i}@${i}`,
         password: bcrypt.hashSync(String(i), 10),
       });
-      });
+  
     }
     queryInterface.bulkInsert("Users", arr);
     queryInterface.bulkInsert("Users", arr);
@@ -27,7 +26,5 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("Users", null, {});
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Users", null, {});
-  },
+
 };
