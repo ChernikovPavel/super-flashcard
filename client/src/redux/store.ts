9120d660d3d58b611a75180d03056d1019c8d2d5
PavelChernickov <chernickov.pasha@gmail.com>
2024-09-06 13:11:51 +0300

    0

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./slices/userSlice";
 import authSlice from "./slices/authSlice";
+import entriesSlice from "./slices/entriesSlice";
 
 export const store = configureStore({
   reducer: {
+    entriesSlice,
     userSlice,
     authSlice,
   },
