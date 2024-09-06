ff25a9dd13f7168d775bd3a278c58f65e2158418
PavelChernickov <chernickov.pasha@gmail.com>
2024-09-06 15:03:24 +0300

    seeds with names

--- server/db/seeders/20240905100711-User.js
+++ server/db/seeders/20240905100711-User.js
@@ -5,10 +5,14 @@ const bcrypt = require("bcrypt");
 module.exports = {
   async up(queryInterface, Sequelize) {
     const arr = [];
-
-    for (let i = 1; i <= 10; i += 1) {
+    const mockNames = ['Maks Gosling', 'Grisha Heisenberg', 'Anton Northon', 'Denis the Punisher']
+    const mixedMockNames = []
+    const salt = Math.floor(Math.random() * 10)
+    const max = salt + mockNames.length 
+    for(let i = salt; i < max; i+=1) mixedMockNames.push(mockNames[i % mockNames.length])
+    for (let i = 1; i <= 4; i += 1) {
       arr.push({
-        name: `${i}${i}`,
+        name: mixedMockNames[i],
         email: `${i}@${i}`,
         password: bcrypt.hashSync(String(i), 10),
       });
